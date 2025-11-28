9928c853f175280f4cac1067afb7c2767f67e1ae
mihailmarkevich <my.maradon@gmail.com>
2025-11-28 22:24:53 +0100

    change client's config

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
    * Base URL of the backend API.
    * Adjust this to point to your ASP.NET Core backend.
    */
-  apiBaseUrl: "https://localhost:5001"
+  apiBaseUrl: "http://localhost:5215"
 };
